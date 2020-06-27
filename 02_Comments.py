661fa1d7e2a38f5bb89ad3328ca7134c85d09a18
Daniel Alves <daniel@wedapp.io>
2020-06-26 21:17:14 -0400

    fixed the name of file 01; creating the file 02; updating README file

---
+++ 02_Comments.py
@@ -0,0 +1,16 @@
+# This is a comment
+# This will print the word Hello
+print("Hello")
+# This will print the word World
+print("World")
+# This will print ¡!
+print("¡!")
+
+# Multi line comments, with """ before and after
+"""
+This part of code will not be considered
+    print("Hello")
+    print("World")
+    print("¡!")
+"""
+print("End of multi line comments")
--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 ### Python
 Here you'll find my lessons and exercises about Python.
 
-### 1
+### 01
 "Hello World"
 
+### 02
+File showing examples of comments.
 
 ## WEDAPP
 ### Daniel Alves
